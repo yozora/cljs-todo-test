023616e2c531cce19cc0d4d3dbadadb5b55ef8d5
Yozora <yozora@gmail.com>
2015-06-06 13:22:32 -0400

    Refactoring for AJAX communication and adding AJAX stub

--- src/cljs/todo_mvc/core.cljs
+++ src/cljs/todo_mvc/core.cljs
@@ -2,8 +2,9 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
 			[clojure.string :as string]
-            [cljs.core.async :refer [put! <! chan]]
+            [cljs.core.async :refer [chan put! <! close!]]
             [sablono.core :as html :refer-macros [html]]
+            [goog.net.XhrIo :as xhr]
             [om.dom :as dom])
   (:import [goog.ui IdGenerator]))
 
@@ -20,15 +21,32 @@
   (.getNextUniqueId (.getInstance IdGenerator)))
 
 ;; Handlers
+(defn ajax [url payload]
+  (let [ch (chan 1)]
+    (xhr/send url
+              (fn [event]
+                (let [res (-> event .-target .getResponseText)]
+                  (put! ch res)
+                  (close! ch)))
+              "POST"
+              payload)
+    ch))
 
 (defn destroy-todo [app {:keys [id]}]
   (om/transact! app :todos
     (fn [todos] (into [] (remove #(= (:id %) id) todos))))
 )
 
+(defn toggle-completed [item]
+  (om/transact! item :completed not))
+
+(defn todo-add [app todo]
+  (om/transact! app :todos #(conj % todo)))
+
 (defn handle-message [{:keys [type data] :as message} app]
   (case type
     :destroy (destroy-todo app data)
+    :toggle (toggle-completed data)
     nil))
     
 (def ENTER_KEY 13)
@@ -39,38 +57,38 @@
           text (.-value todo-field)]
       (when-not (string/blank? text)
         (let [newtodo {:id (guid) :text text :completed false}]
-          (om/transact! app :todos #(conj % newtodo)
-          (set! (.-value todo-field) "")))))))
+          (todo-add app newtodo)
+          (set! (.-value todo-field) ""))))))
 
 ;; Components
 
-(defn todo-list-item [item owner]
+(defn todo-list-item [item owner {:keys [comm]}]
   (reify
     om/IDisplayName
     (display-name [_] "todo-list-item")
-    om/IRenderState
-    (render-state [_ {:keys [comm]}] (html
+    om/IRender
+    (render [_] (html
       [:li {:className (if (:completed item) "completed" "")}
         [:div {:className "view"}
           [:input {:type "checkbox"
                    :className "toggle"
-                   :on-change #(om/transact! item :completed not)
+                   :on-change #(put! comm {:type :toggle :data item})
                    :checked (and (:completed item) "checked")}]
           [:label (:text item)]
           [:button {:className "destroy"
    					:on-click (fn [_] (put! comm {:type :destroy :data item}) nil)}]]]))))
 
-(defn top-app [{:keys [todos] :as app} owner]
+(defn top-app [{:keys [todos] :as app} owner {:keys [comm]}]
   (reify
     om/IDisplayName
     (display-name [_] "top-app")
-    om/IRenderState
-    (render-state [_ {:keys [comm]}] (html
+    om/IRender
+    (render [_] (html
       [:section {:id "top-app" :style (if (empty? todos) {:display "none"} {}) }
         [:input {:id "toggle-all" :type "checkbox"}]
         (into
           [:ul {:id "todo-list"}]
-          (om/build-all todo-list-item todos {:init-state {:comm comm} :key :id}))]))))
+          (om/build-all todo-list-item todos {:opts {:comm comm} :key :id}))]))))
 
 (defn todo-app [{:keys [todos] :as app} owner]
   (reify
@@ -92,7 +110,7 @@
                     :ref "todo-text"
                     :onKeyDown #(handle-entry-keydown % app owner)
                     :placeholder (L "What needs to be done?")}]]
-        (om/build top-app app {:init-state {:comm comm}})]))
+        (om/build top-app app {:opts {:comm comm}})]))
 ))
 
 (defn main []
